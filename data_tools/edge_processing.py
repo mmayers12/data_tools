afc3f073c62dd9fd8d3f3bb8f28e585e9d2b70f3
Mike Mayers <mmayers@scripps.edu>
2019-10-29 12:58:29 -0700

    add tqdm option, and drop untyped edges

--- data_tools/edge_processing.py
+++ data_tools/edge_processing.py
@@ -1,7 +1,11 @@
 import pandas as pd
+from tqdm import tqdm
 
 
 def change_edge_type(edges, from_type, to_type, swap=False):
+    """
+    In-place change of an edge type in a hetnet file.
+    """
     idx = edges.query('type == @from_type').index
     edges.loc[idx, 'type'] = to_type
     if swap:
@@ -11,14 +15,23 @@ def change_edge_type(edges, from_type, to_type, swap=False):
 
 
 def map_edge_types_from_file(edges, map_df, orig_type='type', new_type='new_type',
-                             swap_label='reverse_node_labels'):
+                             swap_label='reverse_node_labels', prog=True):
     """
     In-place updater of Edge Types from a mapping dataframe
     """
-    for row in map_df.itertuples():
+
+    def inner_func():
         from_type = getattr(row, orig_type)
         to_type = getattr(row, new_type)
         swap = getattr(row, swap_label)
 
         change_edge_type(edges, from_type, to_type, swap)
 
+    if prog:
+        for row in tqdm(map_df.itertuples(), total=len(map_df)):
+            inner_func()
+    else:
+        for row in map_df.itertuples():
+            inner_func()
+
+    edges.dropna(subset=['type'], inplace=True).reset_index(drop=True, inplace=True)
