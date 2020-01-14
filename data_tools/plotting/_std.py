c02dc7277b1695ab3525218ff0817ba83d815a3d
Mike Mayers <mmayers@scripps.edu>
2020-01-14 11:11:35 -0800

    allow user supplied ordering

--- data_tools/plotting/_std.py
+++ data_tools/plotting/_std.py
@@ -4,7 +4,7 @@ import seaborn as sns
 
 __all__ = ['count_plot_h']
 
-def count_plot_h(data, annotate=True, **params):
+def count_plot_h(data, annotate=True, order=None, **params):
     """
     Wrapper for seaborn's Barpolot with horizontal result. Functions like sns.countplot(), but also allows for
     the result of Series.value_counts() to be passed as an argument.
@@ -14,12 +14,16 @@ def count_plot_h(data, annotate=True, **params):
     :param annotate: Boolean, dict, or Series. If `True` will plot the counts at the end of the bars. If `False`,
         no annotations will be plotted. If a Series or dict, annotations other than the values can be passed
         and plotted at the end of the bars.  (e.g. plot percentages, but annotate with counts).
+    :param order: list, override the most-to-least (top to bottom) order with a user supplied order.
     :**params: Other arguments to be passed to Seaborn's Barplot function.
     """
     # TODO: Better checks (does't work if obj type and/or nan values)
     # Do the value counts if not already done....
     if data.dtype not in [int, float]:
         data = data.value_counts()
+
+    # Ensure the data is properly sorted
+    data = data.sort_values(ascending=False)
 
     # Allow for numeric indices (sometimes we want counts of counts)
     if data.index.dtype in [int, float]:
@@ -28,8 +32,12 @@ def count_plot_h(data, annotate=True, **params):
     else:
         y = data.index
 
+    # Allow for a user supplied order
+    if order is None:
+        order = y
+
     # Plot the data orderred from most to least frequent
-    splot = sns.barplot(x=data, y=y, order=y, **params)
+    splot = sns.barplot(x=data, y=y, order=order, **params)
 
     # Allow a series to be passed, to annotate with different values
     if type(annotate) == pd.Series:
